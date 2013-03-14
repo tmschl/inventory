b20145f40fb3c7eee5df272e11effe6bd0f0d613
Robb Schiller <robb.schiller@gmail.com>
2013-03-13 20:15:52 -0400

    manual merge...?

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express')
+  , routes = require('./routes/routes.js');
+
 var app = express();
 
-app.get('/', function(req, res){
-  res.send('Hello World');
-});
+app.get('/', routes.index);
 
 app.listen(3000);
 console.log('Listening on port 3000');
---
+++ routes/routes.js
@@ -0,0 +1,6 @@
+/* Is the route supposed to get the homepage?
+*/
+
+exports.index = function(req, res){
+  res.send('Hello World');
+};
